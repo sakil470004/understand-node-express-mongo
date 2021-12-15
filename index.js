59943e40c2233093722f43f1497b82e8e11c11fc
Mynul Islam <sk470004@gmail.com>
2021-12-15 07:34:47 +0600

    This repo for start up perpose

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config()
 
 const app = express()
-const port = 5000;
+const port = process.env.PORT||5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
